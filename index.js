ef5d7b5160fcdea73888c8ac9691d95d6266cfc7
Chris Smith <me@chrisryansmith.com>
2018-11-26 22:39:29 -0500

    add commander npm package and setup initial index.js package showing connection to rinkeby through infura+web3

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+
+const Web3 = require('web3');
+const web3 = new Web3('https://rinkeby.infura.io/v3/c65b94c5bb2946b5b3a1bd0c084e8ac5');
+
+program
+  .version('0.1.0')
+  .option('-b, --blockNumber <n>', 'Block number to search from')
+  .option('-s, --startBlock <n>', 'Starting block number')
+  .option('-e, --endBlock <n>', 'End block number (inclusive)')
+  .parse(process.argv);
+
+const run = async () => {
+  console.log('starting program');
+  const blockNumber = await web3.eth.getBlockNumber();
+  console.log('Current blockNumber', blockNumber);
+  console.log(program.blockNumber, program.startBlock, program.endBlock);
+};
+
+run();
