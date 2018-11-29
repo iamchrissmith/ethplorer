97b5515acc81b57581d0e082c629508abb3cdc7a
Chris Smith <me@chrisryansmith.com>
2018-11-28 22:34:30 -0500

    add initial test for block object

---
+++ test/Blocks.test.js
@@ -0,0 +1,26 @@
+const {assert} = require('chai');
+
+const Block = require('../models/Block.js');
+const FakeWeb3 = require('./mocks/FakeWeb3.js');
+
+describe('Block', function() {
+  let block;
+  let fakeData;
+
+  beforeEach(() => {
+    fakeData = {
+      number: 1,
+    }
+    block = new Block(fakeData);
+  });
+
+  afterEach(() => {
+  })
+
+  describe('constructor', () => {
+    it('it should save data', async () => {
+      assert.isObject(block.data);
+      assert.equal(block.data.number, fakeData.number);
+    });
+  });
+});
