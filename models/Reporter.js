83ec7f2228bedd70ce292aee0ede5fc0c5bc362b
Chris Smith <me@chrisryansmith.com>
2018-12-02 14:49:32 -0500

    add recipient and sender tracking

--- models/Reporter.js
+++ models/Reporter.js
@@ -5,8 +5,8 @@ module.exports = class Reporter {
     this.txs = _txs;
     this.results = {
       total: BN(0),
-      recipients: [],
-      senders: [],
+      recipients: {},
+      senders: {},
     }
   }
 
@@ -14,6 +14,20 @@ module.exports = class Reporter {
     for(let i = 0; i < this.txs.length; i++){
       const tx = this.txs[i].data;
       this.results.total = this.results.total.plus(tx.value);
+      this.results.recipients[tx.to] = {
+        wei: 
+          this.results.recipients[tx.to] ? 
+          this.results.recipients[tx.to].wei.plus(tx.value) : 
+          new BN(0).plus(tx.value),
+        contract: false
+      };
+      this.results.senders[tx.from] = {
+        wei: 
+          this.results.senders[tx.from] ? 
+          this.results.senders[tx.from].wei.plus(tx.value) : 
+          new BN(0).plus(tx.value),
+        contract: false
+      };
     }
     return this.results;
   }
--- test/Reporter.test.js
+++ test/Reporter.test.js
@@ -17,31 +17,27 @@ describe('Reporter', function() {
     fakeTxs = [fakeTx, fakeTx1];
     reporter = new Reporter(fakeTxs);
     expectedResults = {
-      total: new BN(1000000000000000000).plus(500000000000000000),
-      recipients: [
-        {
-          address: '0xbcd24b757c08843010b94cda1f8d03b90b167313',
+      total: new BN(fakeTx.data.value).plus(fakeTx1.data.value),
+      recipients: {
+        '0x27669D192b5bc0E37Da1D2fDb5eDE5d5bBC695b6': {
           wei: new BN(1000000000000000000),
           contract: false,
         },
-        {
-          address: '0x0000000000000000000000000000000000000000',
+        '0x0000000000000000000000000000000000000000': {
           wei: new BN(500000000000000000),
           contract: false,
         },
-      ],
-      senders: [
-        {
-          address: '0x9762f6858ccae9ca006485fe1a9e6820a44419b1',
-          wei: new BN(-1000000000000000000),
+      },
+      senders: {
+        '0x7BC658E83A94bff25d69E8a3ac289aA3b4D539B9': {
+          wei: new BN(1000000000000000000),
           contract: false,
         },
-        {
-          address: '0x27669D192b5bc0E37Da1D2fDb5eDE5d5bBC695b6',
-          wei: new BN(-500000000000000000),
+        '0x27669D192b5bc0E37Da1D2fDb5eDE5d5bBC695b6': {
+          wei: new BN(500000000000000000),
           contract: false,
         },
-      ],
+      },
     }
   });
 
@@ -62,13 +58,24 @@ describe('Reporter', function() {
       assert.property(result, 'total');
       assert.instanceOf(result.total, BN);
       assert.property(result, 'recipients');
-      assert.isArray(result.recipients);
+      assert.isObject(result.recipients);
       assert.property(result, 'senders');
-      assert.isArray(result.senders);
+      assert.isObject(result.senders);
     });
+
     it('it should sum ether', async () => {
       const result = await reporter.build();
       assert.equal(result.total.toString(), expectedResults.total.toString());
+    });
+
+    it('it should report recipients', async () => {
+      const result = await reporter.build();
+      assert.deepEqual(result.recipients, expectedResults.recipients);
+    });
+
+    it('it should report senders', async () => {
+      const result = await reporter.build();
+      assert.deepEqual(result.senders, expectedResults.senders);
     });
   });
 
